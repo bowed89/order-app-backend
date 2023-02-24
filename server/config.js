7ea43dd172689c4382d595e91d95938ff1129cee
bowed89 <jesus.lopez.averanga@gmail.com>
2023-02-23 21:38:53 -0400

    bgbgbg

--- server/config.js
+++ server/config.js
@@ -35,11 +35,11 @@ const con = new Client({
 
 
 const con = new Client({
-    user: 'qikytpyukyaxtx',
-    host: 'ec2-34-199-68-114.compute-1.amazonaws.com',
-    database: 'd3ctrjqk22uavb',
-    password: '79051d1d17607a9702f4ad6dafcadaefe3609b59b26a5eb0b2b3449b6ad16c12',
-    port: 5432,
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port
 });
 
 (async () => {
