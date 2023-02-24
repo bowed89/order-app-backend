073cdfc06fe90ef2f393acb965e47f384b46c1af
bowed89 <jesus.lopez.averanga@gmail.com>
2023-02-24 18:36:09 -0400

    change Database

--- server/config.js
+++ server/config.js
@@ -31,21 +31,26 @@ const con = new Client({
 });
 
 //con.connect(); 
-*/
 
-
-const con = new Client({
+ const con = new Client({
     user: process.env.user,
     host: process.env.host,
     database: process.env.database,
     password: process.env.password,
     port: process.env.port
+}); */
+
+const con = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 (async () => {
     try {
         await con.connect();
-        console.log('connected');
+        console.log('connecteded');
     } catch (e) {
         console.log('Error happend while connecting to the DB: ', e.message)
     }
