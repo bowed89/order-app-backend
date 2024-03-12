e361e201178067a0e8cd5a3dfd9fc65e45485718
bowed89 <jesus.lopez.averanga@gmail.com>
2024-03-12 00:22:52 -0400

    fifth commit

--- server/config.js
+++ server/config.js
@@ -1,14 +1,5 @@
 const { Client } = require('pg');
 
-// Conexion BD RAILWAY
-/* const con = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-con.connect(); */
-
 //Conexion con BD LOCAL
 /* const con = new Client({
     user: 'postgres',
@@ -21,31 +12,13 @@ con.connect(); */
 con.connect();  */
 
 // Conexion BD RAILWAY
-/* try {
-    const con = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-        }
-    });
-
-    con.connect();
-    module.exports = con;
-
-} catch (error) {
-    console.error('Error al conectar a la base de datos:', error);
-}
- */
-
-
-// Conexion BD con host, username y contraseña
 try {
     const con = new Client({
-        user: 'postgres',
-        host: 'roundhouse.proxy.rlwy.net',
-        database: 'CHUPITOS_DB',
-        password: 'ZdoWGkZBcCjmgjozEcwckEtMGoMQqehQ',
-        port: '56773', // Por defecto es 5432
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD,
+        port: process.env.PORT
     });
 
     con.connect();
