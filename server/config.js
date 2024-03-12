0dbac428f61d0c6e871f3ceb46faae80f66822d2
bowed89 <jesus.lopez.averanga@gmail.com>
2024-03-12 00:32:51 -0400

    fifth commit

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+require('dotenv').config(); // Cargar las variables de entorno desde el archivo .env
 
 //Conexion con BD LOCAL
 /* const con = new Client({
@@ -14,11 +15,11 @@ con.connect();  */
 // Conexion BD RAILWAY
 try {
     const con = new Client({
-        user: 'postgres',
-        host: 'roundhouse.proxy.rlwy.net',
-        database: 'CHUPITOS_DB',
-        password: 'ZdoWGkZBcCjmgjozEcwckEtMGoMQqehQ',
-        port: '56773', // Por defecto es 5432
+        user: process.env.USER,
+        host: process.env.HOST,
+        database: process.env.DATABASE,
+        password:process.env.PASSWORD,
+        port: process.env.PORT,
     });
 
     con.connect();
@@ -27,3 +28,9 @@ try {
 } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
 }
+
+
+
+
+
+
